57d4ae2e33724c60908fb340bece1ef95dc4ed94
jellyseop <jellyjun7777@naver.com>
2025-11-22 15:03:09 +0900

    Day 33  Project 6 - 1e

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by junseopLee on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
