530c8b4bd09c4a1516f005e2c1253678c49f5f7d
jellyseop <jellyjun7777@naver.com>
2025-12-03 22:21:27 +0900

    Day 50 Project 10, part 2

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by junseopLee on 12/3/25.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
